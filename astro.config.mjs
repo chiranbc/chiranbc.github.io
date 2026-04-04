f760721902161218e3f5e89ce5af4a0ed866af3d
Chiran Binnu Cherian <chirancherian@gmail.com>
2026-04-04 14:49:33 -0500

    Update to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,9 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-
-import { template } from "./src/settings";
-
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-    integrations: [react(), tailwind(), sitemap()],
-    site: template.website_url,
-    base: template.base,
+  integrations: [react(), tailwind(), sitemap()],
+  site: "https://chiranbc.github.io",
 });
